ce377179d71ac2732db050f58191dbe5ebd00a56
AralCA <aralcavlak@gmail.com>
2023-12-09 20:40:56 +0300

    Completed the deck process.

--- Scripts/mainScript.java
+++ Scripts/mainScript.java
@@ -19,22 +19,43 @@ public class mainScript {
     public static void Play(){
         GameDeck = createDeck("main");
         PlayerDeck = createDeck("player");
-        CompDeck = createDeck("Comp");
+        CompDeck = createDeck("comp");
         //Decks Done
         System.out.println("Decks have been generated.");
 
-        for (card card1 : shuffler(GameDeck)) {
-            System.out.println(card1.num);
+        /*for (card card1 : shuffler(GameDeck)) {
+            //System.out.println(card1.num);
         }
+        */
+
+        mixAndShuffle();
+        PlayerDeck = completeDeck(PlayerDeck);
+        CompDeck = completeDeck(CompDeck);
+        
+
+        System.out.println(PlayerDeck[9].);
+
     }
 
     public static card[] createDeck(String deckName){
 
         if(deckName == "main"){
-            card[] returnMainDeck= new card[10];
+            card[] returnMainDeck= new card[40];
+
+            int currColorCount = 0;
+            String currColorName = "";
             
-            for(int i = 1;i<11;i++)
-            returnMainDeck[i-1] = new card("main","main",true,i);
+            for(int i = 0;i<40;i++){
+                
+                currColorName = colorInterpertation(currColorCount);
+                
+
+                returnMainDeck[i] = new card(currColorName,"main",true,i%10+1);
+
+                if(((i%10)+1)==0) currColorCount++;
+            }
+
+
 
             return returnMainDeck;
         }else{
@@ -45,6 +66,13 @@ public class mainScript {
     }
 
     public static void mixAndShuffle(){
+        GameDeck = shuffler(GameDeck);
+
+        for(int i=0; i<10; i++){
+            PlayerDeck[i] = GameDeck[i];
+            CompDeck[i] = GameDeck[GameDeck.length-i-1];
+        }
+
 
     }
 
@@ -65,12 +93,61 @@ public class mainScript {
                 lastRandom = rd.nextInt(deckSize);
             }while(finalDeck[lastRandom].num!=0);
             
-
             finalDeck[lastRandom] = cards;
             count--;
         }
         System.out.println("done");
         return finalDeck;
     }
+
+    public static card[] completeDeck(card[] deck){
+        Random rd = new Random(Calendar.getInstance().getTimeInMillis());
+        
+        for(int i = 0; i<5; i++){
+            
+            int tempColor = rd.nextInt(4);
+            int tempNum = rd.nextInt(6)+1;
+            Boolean isPos = rd.nextBoolean();
+
+            String tempColorName = colorInterpertation(tempColor);
+
+            deck[i+5]= new card(tempColorName,deck[0].deck,isPos,tempNum);
+
+        }
+
+        if(rd.nextInt(5)==0){
+            deck[8] = new card("DOUBLE",deck[0].deck,true,0);
+        }
+        if(rd.nextInt(5)==0){
+            deck[9] = new card("FLIP",deck[0].deck,true,0);
+        }
+
+
+        return deck;
+    }
+
+    public static String colorInterpertation(int colorNum){
+        String currColorName;
+        switch(colorNum){
+            case 0:
+                currColorName = "Red";
+                break;
+            case 1:
+                currColorName = "Blue";
+                break;
+            case 2:
+                currColorName = "Yellow";
+                break;
+            case 3:
+                currColorName = "Green";
+                break;
+            default:
+                currColorName = "Red";
+                break;
+
+        }
+        return currColorName;
+    }
+
     
 }
